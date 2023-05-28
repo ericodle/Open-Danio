f4a5710f2acb8afdd1afa6720cc595ea4825bc56
EricOdle <72859331+ericodle@users.noreply.github.com>
2023-05-28 19:11:05 +0900

    Update novel_tank_dive.py

--- novel_tank_dive.py
+++ novel_tank_dive.py
@@ -1,143 +1,157 @@
-# Fish height during the novel tank dive can be an indicator of anxiety.
-# Users can obtain a framewise list of Y coordinates from their preferred open-source tracking software.
-# Import required libraries.
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from numpy import genfromtxt
-import PIL
-import os
+import matplotlib.pyplot as plt
 
-# Enter the file path and name for the Y coordinate data.
-# Note: Ensure that your video footage was captured with an orientation in which the target axis represent height.
+def load_csv(file_path):
+    """
+    Load the CSV file containing the XY pixel positions.
 
-my_path = 'path_to_Y_coordinates/'
-my_file = 'file.csv'
+    Args:
+        file_path (str): Path to the CSV file.
 
-# Convert csv data into a list, then into a numpy array.
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded data.
+    """
+    return pd.read_csv(file_path)
 
-y_vals = genfromtxt(my_path+my_file, delimiter=',')
-y_array=np.array(y_vals)
 
-# Remove NaN values
+def define_zones(y_values):
+    """
+    Define the zones based on the Y values.
 
-y_result = y_array[np.logical_not(np.isnan(y_array))]
+    Args:
+        y_values (pd.Series): Series containing the Y values.
 
-# Determine min and max Y values to identify range
-# We assume that the fish swam to both the bottom and top of the tank at least once during the tracking period.
-# Fish that fail to swim to both the bottom and top of the tank should be excluded.
+    Returns:
+        list: List of zone boundaries.
+    """
+    max_y = y_values.max()
+    min_y = y_values.min()
+    zone_height = (max_y - min_y) / 3
 
-y_min = np.min(y_result)
-y_max = np.max(y_result)
+    zone_boundaries = [min_y + zone_height, min_y + (2 * zone_height)]
+    return zone_boundaries
 
-# Subtract max-min to get range. 
-# Smaller values are higher up. Image coordinates originate at top left corner.
-# Specifically, the variable "y_max" represents the bottom of the tank, and vice versa.
 
-y_range = y_max-y_min
+def calculate_path_zones(data):
+    """
+    Calculate the travel path zones and plot the path with zones overlaid.
 
-# Multiply y_range by .33333 to determine 1/3 length of y_range.
-# Our novel tank dive considered three zones (bottom, middle, and top). Other protocols only consider two zones.
+    Args:
+        data (pd.DataFrame): DataFrame containing the XY pixel positions.
 
-zone_size = y_range * .33333
+    Returns:
+        tuple: A tuple containing the time spent in each zone.
+    """
+    y = data['Y']
+    zone_boundaries = define_zones(y)
 
-# Define the lower boundary height of the top zone.
+    zone_1_time = 0
+    zone_2_time = 0
+    zone_3_time = 0
 
-top_zone = y_min + zone_size
+    # Plotting the path with zones
+    plt.figure(figsize=(10, 6))
+    plt.plot(data['X'], data['Y'], color='blue', label='Path')
+    plt.axhline(zone_boundaries[0], color='red', linestyle='--', label='Zone 1')
+    plt.axhline(zone_boundaries[1], color='green', linestyle='--', label='Zone 2')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Path with Zones')
+    plt.legend()
 
-# Define minute intervals. Video was recorded at 60 fps. Therefore, one minute is 3600 frames.
-# If user video was captured at a different frame rate, adjust these intervals accordingly.
-# Note: When tracking, it is helpful to ensure that tracking time corresponds to actual test start time as closely as possible.
-# Note: Any acclimation period is assumed to have taken place before frame zero. Adjust your protocol accordingly.
+    prev_zone = None
 
-min1 = y_result[0:3600]
-min2 = y_result[3601:7200]
-min3 = y_result[7201:10800]
-min4 = y_result[10801:14400]
-min5 = y_result[14401:18000]
+    for index, row in data.iterrows():
+        curr_y = row['Y']
+        curr_zone = None
 
-# Plot the framewise change in swim depth for each test minute.
-# Using this plot, the number of top zone entries can be manually counted.
-# Plot details can be adjusted
+        if curr_y <= zone_boundaries[0]:
+            curr_zone = 1
+            zone_1_time += row['Time']
+        elif curr_y <= zone_boundaries[1]:
+            curr_zone = 2
+            zone_2_time += row['Time']
+        else:
+            curr_zone = 3
+            zone_3_time += row['Time']
 
-plt.gca().invert_yaxis()
-plt.plot(min1, color='magenta', linewidth='1',mfc='pink' ) # Adjust color and line width
-plt.xticks(range(0,len(min1)+1, 60))
-plt.ylabel('Y position') # Set label for Y axis
-plt.xlabel('time (120 frames per point)') # Set label for X axis
-plt.axhline(y=top_zone)
-plt.title("minute1") # Set title of plot
-plt.savefig(my_path+'Minute_1.tif') #Save plot to same folder as original data.
-plt.show() # This line can be commented out or deleted if user does not desire immediate viewing.
+        if prev_zone is not None and curr_zone != prev_zone:
+            plt.scatter(row['X'], row['Y'], color='black', marker='x')
 
-# Repeat for minutes 2 through 5
+        prev_zone = curr_zone
 
-plt.plot(min2, color='magenta', linewidth='1',mfc='pink' )
-plt.xticks(range(0,len(min2)+1, 60))
-plt.ylabel('Y position') #set the label for y axis
-plt.xlabel('time (120 frames per point)') #set the label for x-axis
-plt.axhline(y=top_zone)
-plt.title("minute2") #set the title of the graph
-plt.savefig(my_path+'Minute_2.tif')
-plt.show()
+    plt.show()
 
-plt.plot(min3, color='magenta', linewidth='1',mfc='pink' )
-plt.xticks(range(0,len(min3)+1, 60))
-plt.ylabel('Y position') #set the label for y axis
-plt.xlabel('time (120 frames per point)') #set the label for x-axis
-plt.axhline(y=top_zone)
-plt.title("minute3") #set the title of the graph
-plt.savefig(my_path+'Minute_3.tif')
-plt.show()
+    return zone_1_time, zone_2_time, zone_3_time
 
-plt.plot(min4, color='magenta', linewidth='1',mfc='pink' )
-plt.xticks(range(0,len(min4)+1, 60))
-plt.ylabel('Y position') #set the label for y axis
-plt.xlabel('time (120 frames per point)') #set the label for x-axis
-plt.axhline(y=top_zone)
-plt.title("minute4") #set the title of the graph
-plt.savefig(my_path+'Minute_4.tif')
-plt.show()
+def calculate_zone_transitions(data):
+    """
+    Calculate the zone transitions based on the XY coordinate data.
 
-plt.plot(min5, color='magenta', linewidth='1',mfc='pink' )
-plt.xticks(range(0,len(min5)+1, 60))
-plt.ylabel('Y position') #set the label for y axis
-plt.xlabel('time (120 frames per point)') #set the label for x-axis
-plt.axhline(y=top_zone)
-plt.title("minute5") #set the title of the graph
-plt.savefig(my_path+'Minute_5.tif')
-plt.show()
+    Args:
+        data (pd.DataFrame): DataFrame containing the XY pixel positions.
 
-# Determine minute-wise time in top zone
-# Identify the frames during which the fish was within the top zone.
+    Returns:
+        dict: Dictionary containing the count of zone transitions.
+    """
+    y = data['Y']
+    zone_boundaries = define_zones(y)
 
-min1_top = min1[min1<top_zone]
-min2_top = min2[min2<top_zone]
-min3_top = min3[min3<top_zone]
-min4_top = min4[min4<top_zone]
-min5_top = min5[min5<top_zone]
+    transitions = {
+        'zone1_to_zone2': 0,
+        'zone2_to_zone1': 0,
+        'zone2_to_zone3': 0,
+        'zone3_to_zone2': 0,
+        'zone3_to_zone1': 0,
+        'zone1_to_zone3': 0,
+    }
 
-# Count the number of top zone frames.
+    prev_zone = None
 
-min1_size = np.size(min1_top)
-min2_size = np.size(min2_top)
-min3_size = np.size(min3_top)
-min4_size = np.size(min4_top)
-min5_size = np.size(min5_top)
+    for index, row in data.iterrows():
+        curr_y = row['Y']
+        curr_zone = None
 
-# Divide each minute by 60 (fps) to determine top zone time in seconds.
+        if curr_y <= zone_boundaries[0]:
+            curr_zone = 1
+        elif curr_y <= zone_boundaries[1]:
+            curr_zone = 2
+        else:
+            curr_zone = 3
 
-min1_time = min1_size/60
-min2_time = min2_size/60
-min3_time = min3_size/60
-min4_time = min4_size/60
-min5_time = min5_size/60
+        if prev_zone is not None:
+            if curr_zone == 2 and prev_zone == 1:
+                transitions['zone1_to_zone2'] += 1
+            elif curr_zone == 1 and prev_zone == 2:
+                transitions['zone2_to_zone1'] += 1
+            elif curr_zone == 3 and prev_zone == 2:
+                transitions['zone2_to_zone3'] += 1
+            elif curr_zone == 2 and prev_zone == 3:
+                transitions['zone3_to_zone2'] += 1
+            elif curr_zone == 1 and prev_zone == 3:
+                transitions['zone3_to_zone1'] += 1
+            elif curr_zone == 3 and prev_zone == 1:
+                transitions['zone1_to_zone3'] += 1
 
-# Save these values to a csv.
+        prev_zone = curr_zone
 
-results_header=('Min_1','Min_2','Min_3','Min_4','Min_5')
-results=(min1_time, min2_time, min3_time, min4_time, min5_time)
-dict= {'Test_Minute':results_header, 'Top_Zone_Time(sec)':results}
-df = pd.DataFrame(dict)
-df.to_csv(path+'top_zone_times.csv')
+    return transitions
+
+
+# Usage example
+file_path = 'path_data.csv'
+data = load_csv(file_path)
+zone_1_time, zone_2_time, zone_3_time = calculate_path_zones(data)
+
+print("Time spent in Zone 1:", zone_1_time)
+print("Time spent in Zone 2:", zone_2_time)
+print("Time spent in Zone 3:", zone_3_time)
+
+zone_transitions = calculate_zone_transitions(data)
+
+print("Transitions from Zone 1 to Zone 2:", zone_transitions['zone1_to_zone2'])
+print("Transitions from Zone 2 to Zone 1:", zone_transitions['zone2_to_zone1'])
+print("Transitions from Zone 2 to Zone 3:", zone_transitions['zone2_to_zone3'])
+print("Transitions from Zone 3 to Zone 2:", zone_transitions['zone3_to_zone2'])
+print("Transitions from Zone 3 to Zone 1:", zone_transitions['zone3_to_zone1'])
+print("Transitions from Zone 1 to Zone 3:", zone_transitions['zone1_to_zone3'])
