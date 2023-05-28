49e7011a4e720f3bdabddd011bc7782337f4f526
EricOdle <72859331+ericodle@users.noreply.github.com>
2023-05-28 18:47:28 +0900

    Update 2D_trajectory.py

--- 2D_trajectory.py
+++ 2D_trajectory.py
@@ -1,37 +1,41 @@
-# This function takes an aligned array of X and Y coordinates and charts a stepwise trajectory for the object.
-# XY coordinates can be obtained from video files using open-source image tracking software such as idtracker.ai or DeepLabCut.
-# Import required libraries
-
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 
-# Enter the file path and name for the Y coordinate data.
-my_path = 'path_to_your_xy_coordinate/'
-my_file = 'file_name.csv'
+def load_csv(file_path):
+    """
+    Load the CSV file containing the XY pixel positions.
 
-# Assumes XY coordinates are organized in adjacent colums in the csv file.
+    Args:
+        file_path (str): Path to the CSV file.
 
-from numpy import genfromtxt
-x_vals = genfromtxt(my_path+my_file, delimiter=',')[0]
-y_vals = genfromtxt(my_path+my_file, delimiter=',')[1]
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded data.
+    """
+    return pd.read_csv(file_path)
 
-# Convert coordinate lists into numpy arrays.
 
-x_array=np.array(x_vals)
-y_array=np.array(y_vals)
+def trace_and_plot_path(data):
+    """
+    Trace and plot the object's path using the XY pixel positions.
 
-# Plot the data.
+    Args:
+        data (pd.DataFrame): DataFrame containing the XY pixel positions.
 
-plt.gca().invert_yaxis()
-plt.plot(x_array, y_array, linewidth=1)
+    Returns:
+        None
+    """
+    x = data['X']
+    y = data['Y']
 
-# Set your plot parameters, particularly the X and Y units, as appropriate.
-# Most open-source animal trackers will automatically output pixel coordinates.
+    plt.plot(x, y)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Object Path')
+    plt.show()
 
-plt.title('Fish Trajectory')
-plt.ylabel('Y position (px)')
-plt.xlabel('X position (px)')
 
-# Save trajectory plot as a tif image file in the same folder as coordinate data.
-plt.savefig(my_path+'XY_trajectory.tif')
+# Usage example
+file_path = 'path_data.csv'
+data = load_csv(file_path)
+trace_and_plot_path(data)
+plt.savefig(file_path+'XY_trajectory.tif')
